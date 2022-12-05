36144f9cef87035234bd260308175ca6dd819c44
Sakina Rampurwala <sakina.s@mvclouds.com>
2022-12-05 16:50:44 +0530

    Ticket No:- VCV-30; Description:- Display all related atendee list in To field , add Parent , name , Firstname , lastname field in Atendee list table and Add Priority field in Action item list Table ; Apply some css to style popup box

--- SubBt/force-app/main/default/aura/sendMeeting/sendMeetingController.js
+++ SubBt/force-app/main/default/aura/sendMeeting/sendMeetingController.js
@@ -1,46 +1,48 @@
 ({
-	doInit : function(component, event, helper) {
-         var email = component.get("v.toEmail");
+    doInit: function(component, event, helper) {
+        console.log('RECORD ID IS' + component.get("v.recordId"));
+        var email = component.get("v.toEmail");
         var cc = component.get("v.CC");
         var subject = component.get("v.Subject");
-		 var action = component.get("c.getHTML");
+        var action = component.get("c.getHTML");
         action.setParams({
-            "EmaiValue" : "test@gmail.com",
-            "ccValue" : cc,
-            "SubjectValue" : subject,
-            "recId" : component.get("v.recordId")
+            "EmaiValue": "test@gmail.com",
+            "ccValue": cc,
+            "SubjectValue": subject,
+            "recId": component.get("v.recordId")
         });
-        action.setCallback(this, function (response) {
+        action.setCallback(this, function(response) {
             //alert(response.getState());
-            if(response.getState() === "SUCCESS"){
-               var result = response.getReturnValue();
-                component.set("v.TextField",result.Innhtmlbody);
-                component.set("v.Subject",result.Innsubject)
-               
+            if (response.getState() === "SUCCESS") {
+                var result = response.getReturnValue();
+                component.set("v.TextField", result.Innhtmlbody);
+                component.set("v.Subject", result.Innsubject)
+
             }
         });
-        $A.enqueueAction(action);   
-	},
-    sendEmail : function(component, event, helper) {
-       console.log(JSON.stringify(component.get("v.selectedToContact")));
-       // alert(JSON.stringify(component.get("v.selectedToContact")));
+        $A.enqueueAction(action);
+    },
+    sendEmail: function(component, event, helper) {
+        console.log(JSON.stringify(component.get("v.selectedToContact")));
+        console.log(component.get("v.selectedToContact"));
+        // alert(JSON.stringify(component.get("v.selectedToContact")));
         var email = component.get("v.toEmail");
         var cc = component.get("v.CC");
         var subject = component.get("v.Subject");
         var action = component.get("c.sendMail");
         action.setParams({
-            "contactList" : component.get("v.selectedToContact"),
-            "ccContactList" : component.get("v.selectedCcContact"),
-            "EmaiValue" : email,
-            "ccValue" : cc,
-            "SubjectValue" : subject,
-            "bodyvalue" : component.get("v.TextField")
+            "atendeeList": component.get("v.selectedToContact"),
+            "ccContactList": component.get("v.selectedCcContact"),
+            "EmaiValue": email,
+            "ccValue": cc,
+            "SubjectValue": subject,
+            "bodyvalue": component.get("v.TextField")
         });
-        action.setCallback(this, function (response) {
-            if(response.getState() === "SUCCESS"){
-               // alert(response.getState());
-                 var result = response.getReturnValue();
-               // alert(result);
+        action.setCallback(this, function(response) {
+            if (response.getState() === "SUCCESS") {
+                // alert(response.getState());
+                var result = response.getReturnValue();
+                // alert(result);
                 var toastEvent = $A.get("e.force:showToast");
                 toastEvent.setParams({
                     "title": "Success!",
@@ -51,9 +53,9 @@
                 $A.get("e.force:closeQuickAction").fire()
             }
         });
-        $A.enqueueAction(action);   
+        $A.enqueueAction(action);
     },
-      cancel: function(component,event,helper){
+    cancel: function(component, event, helper) {
         var dismissActionPanel = $A.get("e.force:closeQuickAction");
         dismissActionPanel.fire();
     },
