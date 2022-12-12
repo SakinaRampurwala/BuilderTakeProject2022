dfcf7b6a8bae021414ab8c73c3bdd031bf2d531a
Sakina Rampurwala <sakina.s@mvclouds.com>
2022-12-12 12:32:53 +0530

    SIF -107 completed

--- SubBt/force-app/main/default/aura/PO_From_Selection_Sheet/PO_From_Selection_SheetHelper.js
+++ SubBt/force-app/main/default/aura/PO_From_Selection_Sheet/PO_From_Selection_SheetHelper.js
@@ -10,17 +10,35 @@
         });
         action.setCallback(this, function(response) {
             var state = response.getState();
-            console.log('State ---> ' + state);            
+            console.log('State ---> ' + state);
 
             if (state == "SUCCESS") {
 
                 var result = response.getReturnValue();
-                console.log("Result ---> ",{result});
+                console.log("Result ---> ", { result });
 
                 component.set('v.optionWrapper', result);
+                // result.forEach(element => {
+                //     element.optionDataList.forEach(res => {
+                //         console.log({ res });
+                //         console.log(res.option.buildertek__Product__r);
+                //         for (var i in res.option.buildertek__Product__r) {
+                //             console.log(res.option.buildertek__Product__r.Name.length);
+                //             if (res.option.buildertek__Product__r.Name.length > 80) {
+                //                 console.log(res.option.buildertek__Product__r.Name.substring(0, 80));
+                //                 // res.option.buildertek__Product__r.Name = res.option.buildertek__Product__r.Name.substring(0, 80);
+
+                //             }
+
+                //         }
+
+                //     });
+
+                // });
+
                 if (result.length != 0) {
                     component.set("v.projectId", result[0].getProjectId);
-                } else{
+                } else {
                     component.set("v.noData", true);
                 }
             } else {
@@ -32,7 +50,7 @@
     },
 
     //===== Description:- This method is used to create purchase order when user click on "create PO" button from Selection sheet =====
-    createPOhelper: function(component, event, helper){
+    createPOhelper: function(component, event, helper) {
         try {
             component.set("v.Spinner", true);
             var recordId = component.get("v.recordId");
@@ -41,20 +59,21 @@
             var selectedRowList = [];
 
             var optionWrapper = component.get("v.optionWrapper");
-            console.log('optionWrapper ==> ',{optionWrapper});
+            console.log('optionWrapper ==> ', { optionWrapper });
 
             optionWrapper.forEach(optionList => {
                 optionList.optionDataList.forEach(optionData => {
-                    // console.log('optionData ==> ',{optionData});
-                    if (optionData.selected == true ) {
-                        // console.log('option ==> ',optionData.option);
+                    // console.log(optionData.option.buildertek__Product__r.Name);
+
+                    if (optionData.selected == true) {
                         selectedRowList.push(optionData.option);
                     }
+
                 });
             });
 
-            console.log('selectedRowList ==> ', {selectedRowList});
-            console.log('Project Id => '+component.get('v.projectId'));
+            console.log('selectedRowList ==> ', { selectedRowList });
+            console.log('Project Id => ' + component.get('v.projectId'));
             if (selectedRowList.length != 0) {
 
                 var action = component.get("c.createPO");
@@ -65,27 +84,27 @@
                 });
                 action.setCallback(this, function(response) {
                     var state = response.getState();
-                    console.log(response.getError());
+                    // console.log(response.getError());
                     var prjId = component.get('v.projectId');
-                    var getError = response.getError();
-                    var setError;
-                    console.log(Object.values(getError));
+                    // var getError = response.getError();
+                    // var setError;
+                    // console.log(Object.values(getError));
 
-                    var object = Object.values(getError);
-                    for (const key in object) {
-                        var item = Object.values(object[key].fieldErrors).forEach(element => {
-                            element.forEach(e => {
-                                console.log(e.statusCode);
-                                setError = e.statusCode;
+                    // var object = Object.values(getError);
+                    // for (const key in object) {
+                    //     var item = Object.values(object[key].fieldErrors).forEach(element => {
+                    //         element.forEach(e => {
+                    //             console.log(e.statusCode);
+                    //             setError = e.statusCode;
 
-                            });
+                    //         });
 
-                        });
+                    //     });
 
-                        console.log({ setError });
-                    }
+                    //     console.log({ setError });
+                    // }
 
-                    console.log(state);
+                    // console.log(state);
 
                     if (state == "SUCCESS") {
                         helper.showToast("Success", "Success", "New PO and PO Line Created.", "5000");
@@ -105,42 +124,42 @@
                         }
 
                     } else {
-                        if (setError == 'STRING_TOO_LONG') {
-                            helper.showToast("Error", "Error", 'Poduct name is too long', "5000");
-                        } else {
-                            helper.showToast("Error", "Error", 'Something went wrong', "5000");
-                        }
+                        // if (setError == 'STRING_TOO_LONG') {
+                        //     helper.showToast("Error", "Error", 'Poduct name is too long', "5000");
+                        // } else {
+                        helper.showToast("Error", "Error", 'Something went wrong', "5000");
+                        // }
                     }
                     component.set("v.Spinner", false);
                     $A.get("e.force:closeQuickAction").fire();
                 });
-                 $A.enqueueAction(action);
+                $A.enqueueAction(action);
             } else {
                 component.set("v.Spinner", false);
                 helper.showToast("Error", "Error", "Please Select Options", "5000");
             }
         } catch (error) {
-            console.log('error --> ',{ error });
+            console.log('error --> ', { error });
         }
     },
 
     selectAll: function(component, event, helper) {
         var optionWrapper = component.get("v.optionWrapper");
-        
-        console.log('optionWrapper ==> ',{optionWrapper});
+
+        console.log('optionWrapper ==> ', { optionWrapper });
         optionWrapper.forEach(optionList => {
-                optionList.optionDataList.forEach(optionData => {
-                    console.log('optionData ==> ',optionData);
-                    optionData.selected=false;
-                    if (optionList.selected == true ) {
-                        console.log('true ====> ');
-                        console.log('optionList ==> ',optionList);
-                        optionData.selected=true;
-                    }
+            optionList.optionDataList.forEach(optionData => {
+                console.log('optionData ==> ', optionData);
+                optionData.selected = false;
+                if (optionList.selected == true) {
+                    console.log('true ====> ');
+                    console.log('optionList ==> ', optionList);
+                    optionData.selected = true;
+                }
             });
-            });
-            component.set("v.optionWrapper",optionWrapper);
-            console.log('End optionWrapper ==> ',{optionWrapper});
+        });
+        component.set("v.optionWrapper", optionWrapper);
+        console.log('End optionWrapper ==> ', { optionWrapper });
     },
 
     //===== Description:- This method is used to show toast message !!====
