59ba8025aa16bc24adf068d4c1a3e2f47e94d887
Sakina Rampurwala <sakina.s@mvclouds.com>
2022-12-13 14:59:29 +0530

    sif 113 code alignment

--- SubBt/force-app/main/default/aura/OptionUpgrade/OptionUpgradeController.js
+++ SubBt/force-app/main/default/aura/OptionUpgrade/OptionUpgradeController.js
@@ -10,98 +10,69 @@
         evt.fire();
     },
     newOption: function(component, event, helper) {
-        try {
-            $A.get("e.c:BT_SpinnerEvent").setParams({ "action": "SHOW" }).fire();
-            component.set('v.openOptionBox', true);
-            var action = component.get('c.getOptionRecord');
-            action.setParams({
-                recordId: component.get('v.recordId')
-            });
-            action.setCallback(this, function(response) {
-                var result = response.getReturnValue();
-                var state = response.getState();
-                if (result != null && state == 'SUCCESS') {
-                    component.set('v.Option', result);
-                }
 
-            });
-            $A.enqueueAction(action);
-            $A.get("e.c:BT_SpinnerEvent").setParams({ "action": "HIDE" }).fire();
-        } catch (e) {
-            var toastEvent = $A.get("e.force:showToast");
-            toastEvent.setParams({
-                "title": "Error!",
-                "message": e,
-                "type": "error",
-                "duration": 5000
-            });
-            toastEvent.fire();
-            $A.get("e.c:BT_SpinnerEvent").setParams({ "action": "HIDE" }).fire();
+        $A.get("e.c:BT_SpinnerEvent").setParams({ "action": "SHOW" }).fire();
+        component.set('v.openOptionBox', true);
+        var action = component.get('c.getOptionRecord');
+        action.setParams({
+            recordId: component.get('v.recordId')
+        });
+        action.setCallback(this, function(response) {
+            var result = response.getReturnValue();
+            var state = response.getState();
+            if (result != null && state == 'SUCCESS') {
+                component.set('v.Option', result);
+            }
 
-        }
+        });
+        $A.enqueueAction(action);
+        $A.get("e.c:BT_SpinnerEvent").setParams({ "action": "HIDE" }).fire();
 
 
     },
     createRecord: function(component, event, helper) {
-        try {
-            $A.get("e.c:BT_SpinnerEvent").setParams({ "action": "SHOW" }).fire();
+        $A.get("e.c:BT_SpinnerEvent").setParams({ "action": "SHOW" }).fire();
+        var getOption = component.get('v.Option');
+        var action = component.get('c.cloneOptionRecord');
+        action.setParams({
+            optionRec: getOption,
+            recordId: component.get('v.recordId')
+        });
+        action.setCallback(this, function(response) {
+            var result = response.getReturnValue();
+            var state = response.getState();
+            if (result != null && state == 'SUCCESS') {
+                var navEvt = $A.get("e.force:navigateToSObject");
+                navEvt.setParams({
+                    "recordId": response.getReturnValue(),
+                    "slideDevName": "detail"
+                });
+                navEvt.fire();
 
-            var getOption = component.get('v.Option');
+                var toastEvent = $A.get("e.force:showToast");
+                toastEvent.setParams({
+                    "title": "Success!",
+                    "message": "Option created successfully",
+                    "type": "success",
+                    "duration": 5000
+                });
+                toastEvent.fire();
 
-            var action = component.get('c.cloneOptionRecord');
-            action.setParams({
-                optionRec: getOption,
-                recordId: component.get('v.recordId')
-            });
-            action.setCallback(this, function(response) {
-                var result = response.getReturnValue();
-                if (result != null) {
-                    var navEvt = $A.get("e.force:navigateToSObject");
-                    navEvt.setParams({
-                        "recordId": response.getReturnValue(),
-                        "slideDevName": "detail"
-                    });
-                    navEvt.fire();
+            } else {
 
-                    var toastEvent = $A.get("e.force:showToast");
-                    toastEvent.setParams({
-                        "title": "Success!",
-                        "message": "Option created successfully",
-                        "type": "success",
-                        "duration": 5000
-                    });
-                    toastEvent.fire();
+                var toastEvent = $A.get("e.force:showToast");
+                toastEvent.setParams({
+                    "title": "Error!",
+                    "message": "Something went wrong",
+                    "type": "error",
+                    "duration": 5000
+                });
+                toastEvent.fire();
 
-                } else {
-
-                    var toastEvent = $A.get("e.force:showToast");
-                    toastEvent.setParams({
-                        "title": "Error!",
-                        "message": "Something went wrong",
-                        "type": "error",
-                        "duration": 5000
-                    });
-                    toastEvent.fire();
-
-                }
-
-            });
-            $A.enqueueAction(action);
-            $A.get("e.c:BT_SpinnerEvent").setParams({ "action": "HIDE" }).fire();
-
-        } catch (e) {
-            var toastEvent = $A.get("e.force:showToast");
-            toastEvent.setParams({
-                "title": "Error!",
-                "message": e,
-                "type": "error",
-                "duration": 5000
-            });
-            toastEvent.fire();
-            $A.get("e.c:BT_SpinnerEvent").setParams({ "action": "HIDE" }).fire();
-        }
-
-
+            }
+        });
+        $A.enqueueAction(action);
+        $A.get("e.c:BT_SpinnerEvent").setParams({ "action": "HIDE" }).fire();
     },
     closePopup: function(component, event, helper) {
         $A.get("e.force:closeQuickAction").fire();
