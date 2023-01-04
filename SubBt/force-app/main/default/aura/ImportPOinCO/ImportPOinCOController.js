3e511cf302eac70fa9d51070ee952e5e3bd2829a
Sakina Rampurwala <sakina.s@mvclouds.com>
2023-01-04 12:38:32 +0530

    BUIL-2910 NEW CHANGES

--- SubBt/force-app/main/default/aura/ImportPOinCO/ImportPOinCOController.js
+++ SubBt/force-app/main/default/aura/ImportPOinCO/ImportPOinCOController.js
@@ -11,7 +11,15 @@
                 console.log(response.getState());
                 console.log(response);
                 var result = response.getReturnValue();
-                console.log(result);
+                var poList = result.purchaseOrderList;
+                var poField = result.POisNull;
+                var message = result.message;
+
+                console.log({ poField });
+                console.log({ poList });
+                console.log({ result });
+
+
                 component.set('v.columns', [
                     { label: 'Name', fieldName: 'linkName', type: 'url', typeAttributes: { label: { fieldName: 'Name' }, target: '_blank', }, sortable: true },
                     { label: 'Vendor', fieldName: 'vendorLinkName', type: 'text', type: 'url', typeAttributes: { label: { fieldName: 'vendorName' }, target: '_blank', } },
@@ -19,30 +27,42 @@
                     { label: 'Total', fieldName: 'buildertek__PO_Total__c', type: 'currency' },
                 ]);
 
-                if (result != null && response.getState() === 'SUCCESS') {
-                    console.log({ result });
-                    if (result.length > 0) {
-                        component.set("v.Spinner", false);
-                        component.set('v.isEmpty', false);
-                        result.forEach(element => {
+                if (poField == false && poList != null) {
+                    component.set("v.Spinner", false);
+                    component.set('v.openModal', true);
+                    component.set('v.isEmpty', false);
+                    if (poList.length > 0 && message != 'noData') {
+                        poList.forEach(element => {
                             element.linkName = '/' + element.Id;
                             element.vendorLinkName = '/' + element.buildertek__Vendor__c;
                             if (element.buildertek__Vendor__c != undefined) {
                                 element.vendorName = element.buildertek__Vendor__r.Name;
                             }
                         });
-
                     } else {
                         let setStyle = document.querySelector('.maindiv');
                         setStyle.style.height = '35vh';
                         setStyle.style.overflow = 'hidden';
                         component.set("v.Spinner", false);
                         component.set('v.isEmpty', true);
-                    }
-                    component.set("v.data", result);
-                };
 
-                if (result == null) {
+                    }
+                    component.set("v.data", poList);
+                } else if (poField == true) {
+                    component.set("v.Spinner", false);
+                    component.set('v.openModal', false);
+                    $A.get("e.force:closeQuickAction").fire();
+                    var toastEvent = $A.get("e.force:showToast");
+                    toastEvent.setParams({
+                        "title": "Error!",
+                        "type": "error",
+                        "message": "Purchase Order is already there",
+                    });
+                    toastEvent.fire();
+
+                }
+
+                if (result == null || message == 'noData') {
                     let setStyle = document.querySelector('.maindiv');
                     setStyle.style.height = '35vh';
                     setStyle.style.overflow = 'hidden';
@@ -50,8 +70,6 @@
                     component.set("v.Spinner", false);
                     component.set('v.isEmpty', true);
                 }
-
-
 
             });
             $A.enqueueAction(action);
@@ -86,7 +104,7 @@
             });
             action.setCallback(this, function(response) {
                 component.set("v.Spinner", false);
-
+                console.log(response.getReturnValue());
                 if (response.getState() === 'SUCCESS' && response.getReturnValue() == 'success') {
                     $A.get("e.force:closeQuickAction").fire();
                     $A.get('e.force:refreshView').fire();
