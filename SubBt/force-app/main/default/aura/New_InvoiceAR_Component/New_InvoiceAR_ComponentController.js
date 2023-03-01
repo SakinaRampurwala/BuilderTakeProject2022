32099a8c4eacb09045754b613d406b86fb9d15e2
sakina rampurwala <sakina.s@mvclouds.com>
2023-03-01 17:12:22 +0530

    BUIL-3058

--- SubBt/force-app/main/default/aura/New_InvoiceAR_Component/New_InvoiceAR_ComponentController.js
+++ SubBt/force-app/main/default/aura/New_InvoiceAR_Component/New_InvoiceAR_ComponentController.js
@@ -1,5 +1,6 @@
 ({
     doInit : function(component, event, helper) {
+        component.set('v.Spinner' , true);
         var action = component.get("c.getFieldSet");
         action.setParams({
             objectName: "buildertek__Billings__c",
@@ -10,6 +11,7 @@
             console.log(response.getState());
             console.log(response.getError());
             if (response.getState() == 'SUCCESS') {
+                component.set('v.Spinner' , false);
                 var allFieldsLabel = JSON.parse(response.getReturnValue());
                 console.log({allFieldsLabel});
 
@@ -19,6 +21,7 @@
             } 
         });
         $A.enqueueAction(action);
+
     },
     Cancel:function(component, event, helper) {
         var workspaceAPI = component.find("workspace");
@@ -30,12 +33,95 @@
             console.log(error);
         });
         $A.get("e.force:closeQuickAction").fire();
-        component.set("v.isOpen", false);
-        window.setTimeout(
-            $A.getCallback(function() {
-                $A.get('e.force:refreshView').fire();
-            }), 1000
-        );
+    },
+    handleSubmit : function(component, event, helper) {
+        console.log('handleSubmit');
+        event.preventDefault();  
+        var fields = event.getParam('fields');
+
+        var data = JSON.stringify(fields);
+
+        console.log('data-->>',{data});
+        component.set('v.Spinner' , true);
+        var action = component.get("c.saveRecord");
+        console.log('-----');
+        action.setParams({
+            "salesInvoiceData": data
+        });
+        action.setCallback(this, function (response) {
+            var state = response.getState();
+            var error = response.getError();
+            console.log('Error =>',{error});
+            console.log('state =>',{state});
+
+            if (state === "SUCCESS") {
+                component.set('v.Spinner' , false);
+
+                console.log('success');
+                console.log(response.getReturnValue());
+                var recordId = response.getReturnValue();
+                console.log('recordId-->>',{recordId});
+                
+                var toastEvent = $A.get("e.force:showToast");
+                toastEvent.setParams({
+                    "type": "Success",
+                    "title": "Success!",
+                    "message": "The record has been created successfully."
+                });
+                toastEvent.fire();
+  
+                var saveNnew = component.get("v.isSaveAndNew");
+                console.log('saveNnew: ' + saveNnew);
+  
+                if(saveNnew){
+                    $A.get('e.force:refreshView').fire();
+                }
+                else{
+                    console.log('---Else---');
+                    console.log('saveAndClose');
+                    var navEvt = $A.get("e.force:navigateToSObject");
+                    navEvt.setParams({
+                        "recordId": recordId,
+                        "slideDevName": "Detail"
+                    });
+                    navEvt.fire();
+                    // component.set("v.parentRecordId", null);
+  
+                    var focusedTabId = '';
+                    var workspaceAPI = component.find("workspace");
+                    workspaceAPI.getFocusedTabInfo().then(function(response) {
+                        focusedTabId = response.tabId;
+                    })
+  
+                    window.setTimeout(
+                        $A.getCallback(function() {
+                            workspaceAPI.closeTab({tabId: focusedTabId});
+                        }), 1000
+                    );
+                }
+            }
+            else if (state === "ERROR") {
+                var toastEvent = $A.get("e.force:showToast");
+                toastEvent.setParams({
+                    "type": "Error",
+                    "title": "Error!",
+                    "message": "Something Went Wrong"
+                });
+                toastEvent.fire();
+                console.log('error', response.getError());
+            }
+        });
+        $A.enqueueAction(action);
+    },
+    handlesaveNnew : function(component, event, helper) {
+        component.set('v.Spinner' , true);
+        component.set("v.isSaveAndNew", true);
+        component.set('v.Spinner' , false);
 
     },
+  
+    saveNnew : function(component, event, helper) {
+        component.set("v.isSaveAndNew", true);
+        console.log('saveNnew');
+    }
 })
