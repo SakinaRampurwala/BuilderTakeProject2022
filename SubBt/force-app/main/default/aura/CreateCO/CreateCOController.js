370d4b8c806158bba6ab926d8fd1531b78866d2f
sakina rampurwala <sakina.s@mvclouds.com>
2023-05-04 10:00:15 +0530

    Retrive Result..........

--- SubBt/force-app/main/default/aura/CreateCO/CreateCOController.js
+++ SubBt/force-app/main/default/aura/CreateCO/CreateCOController.js
@@ -1,4 +1,28 @@
 ({
+    doInit: function(component, event, helper) {
+        // console.log('recordId: ' + component.get("v.recordId"));
+        // component.set("v.Spinner", true);
+        // var action = component.get("c.checkChangeOrder");
+        // action.setParams({
+        //     optionId: component.get("v.recordId")
+        // });
+        // action.setCallback(this, function(response) {
+        //     component.set("v.Spinner", false);
+        //     var state = response.getState();
+        //     console.log({ state });
+        //     var result = response.getReturnValue();
+        //     console.log({ result });
+        //     if (result == true) {
+        //         $A.get("e.force:closeQuickAction").fire();
+        //         helper.showToast("Error", "Error", "CO already exist", "5000");
+        //     }
+        // });
+        // $A.enqueueAction(action);
+
+
+    },
+
+
     createRecord: function(component, event, helper) {
         try {
             component.set("v.Spinner", true);
@@ -14,8 +38,11 @@
                 var result = response.getReturnValue();
                 console.log({ result });
                 if(result=='Error'){
-                    helper.showToast("Error", "Error", "CO is already exist", "5000");
+                    helper.showToast("Error", "Error", "CO already exist", "5000");
+                    $A.get("e.force:closeQuickAction").fire();
                 }else {
+                    $A.get("e.force:closeQuickAction").fire();
+                    $A.get('e.force:refreshView').fire();
                     helper.showToast("Success", "Success", "Your Change Order is created", "5000");
                     var navEvent = $A.get("e.force:navigateToSObject");
                     navEvent.setParams({
@@ -24,7 +51,6 @@
                     navEvent.fire();
                 }
                 component.set("v.Spinner", false);
-                $A.get("e.force:closeQuickAction").fire();
             });
             $A.enqueueAction(action);
         } catch (e) {
